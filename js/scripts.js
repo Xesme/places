a9c67d2dd05b7ea98ad420c04c15a8d7be9d6f18
Philip Putnam and Xia Amendolara <philip@>
2017-01-23 14:26:41 -0800

    debug backend logic, add array split, and prototype, fix html, add frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,35 @@
-function Places(placeName, date, landmarks) {
+// business logic
+function Places(placeName, date, landmarks, tips) {
   this.placeName = placeName;
   this.date = date;
-  this.landmarks = landmarks;
+  this.landmarks = landmarks.split(',');
+  this.tips = tips;
 }
 
 Places.prototype.nameDate = function() {
   return (this.placeName + " " + this.date);
 }
 
-var userLocations = function(string) {
-  var landmarks = string.split(',');
+// User Interface logic
 
-  return landmarks;
-};
+$(document).ready(function(){
+  $("#new-location").submit(function(event){
+    event.preventDefault();
 
-var userInput = new Places("Hawaii", "2017", "Kuauai, shark cove, coffee plantation");
+    var inputPlace = $("input#new-place").val();
+    var inputDate = $("input#new-date").val();
+    var inputLocation = $("input#new-landmark").val();
+    var inputTip = $("textarea#new-tip").val();
 
-var tempStr = "Kuauai, shark cove, coffee plantation";
-var tempArry = userLandmarks(tempStr);
+    var newPlace = new Places(inputPlace, inputDate, inputLocation, inputTip);
 
-tempArry.forEach(function(landmark)  {
-  alert(landmark);
+    $("ul#list-places").append("<li>" + newPlace.nameDate() + "</li>");
+
+    for (var index = 0; index < newPlace.landmarks.length; index += 1 ) {
+      $("ol#output-landmarks").append("<li>" + newPlace.landmarks[index] + "</li>");
+    }
+
+    $(".output-box-hid").append(newPlace.tips);
+
+  });
 });
-
-//
-// alert(userInput.nameDate());
-// alert(userInput.locations);
