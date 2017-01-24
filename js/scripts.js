7a42c738b98e406aeb54459723c309b7db86be17
Philip Putnam and Xia Amendolara <xia@>
2017-01-23 16:02:35 -0800

    add user interface content, (this) jQuery, debug toggle, parent-child, and next traversing

--- js/scripts.js
+++ js/scripts.js
@@ -23,13 +23,40 @@ $(document).ready(function(){
 
     var newPlace = new Places(inputPlace, inputDate, inputLocation, inputTip);
 
-    $("ul#list-places").append("<li>" + newPlace.nameDate() + "</li>");
+    // $("ul#list-places").append("<li>" + newPlace.nameDate() + "</li>");
+    //
+    // for (var index = 0; index < newPlace.landmarks.length; index += 1 ) {
+    //   $("ol#output-landmarks").append("<li>" + newPlace.landmarks[index] + "</li>");
+    // }
+    //
+    // $(".output-box-hid").append(newPlace.tips);
+
+
+    $(".output-box-hid").append("<h2>" + newPlace.nameDate() + "</h2>");
+
+
+    $(".output-box-hid").append("<ol></ol>");
+
 
     for (var index = 0; index < newPlace.landmarks.length; index += 1 ) {
-      $("ol#output-landmarks").append("<li>" + newPlace.landmarks[index] + "</li>");
+      $(".output-box-hid").children().last().append("<li>" + newPlace.landmarks[index] + "</li>");
     }
 
-    $(".output-box-hid").append(newPlace.tips);
+    $(".output-box-hid").children().last().append("<br>" + "<li>Tips: <br>" + inputTip + "</li>" + "<br>");
+
+    $(".output-box-hid").children().last().children().hide();
+
+    $(".output-box-hid h2:last-of-type").click(function() {
+      $(this).next("ol").children("li").toggle();
+      // alert("this toggle doe");
+    });
 
   });
+
+  // $(".output-box").click(function() {
+  //   $(".output-box-hid").toggle();
+  // });
+
+
+
 });
